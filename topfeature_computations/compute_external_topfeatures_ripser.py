fix ripser computation

topfeature_computations/compute_external_topfeatures_ripser.py
@@ -81,12 +81,11 @@ def compute_graph_features(line_idx, attns, pool, tsv_writers):
 
 
 def compute_ripser_features(line_idx, attns, pool, tsv_writers):
-    func_args = []
+    results = []
     for layer, head in itertools.product(range(N_LAYERS), range(N_HEADS)):
-        attn = torch.tensor(attns[layer, head])
-        func_args.append((attn, RIPSER_FEATURES))
+        attn = attns[layer, head]
+        results.append(ripser_features_from_attn(attn, RIPSER_FEATURES))
 
-    results = pool.starmap(ripser_features_from_attn, args)
     row_data = [line_idx]
     for data in results:
         row_data.extend(data)

topfeature_computations/utils/feature_extraction.py
@@ -59,8 +59,16 @@ def remove_inf_barcodes(barcode):
 
 
 def ripser_features_from_attn(attn, used_features, maxdim=1):
-    data = rpp_py.run(f"--dim={maxdim} --format=point-cloud", attn)
-    barcode = data["dgms"]
+    data = rpp_py.run(f"--dim {maxdim} --format point-cloud", attn)
+
+    barcode = []
+    for i in range(maxdim + 1):
+        if len(data[i]):
+            arr = data[i].view(np.float32).reshape(data[i].shape + (-1,))
+        else:
+            arr = np.empty(shape=(0, 0))
+        barcode.append(arr)
+
     barcode = remove_inf_barcodes(barcode)
 
     features = []