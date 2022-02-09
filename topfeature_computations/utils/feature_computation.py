bug fixes in feature computation

topfeature_computations/utils/feature_computation.py
@@ -12,11 +12,13 @@ def count_stat(g, func, cap=500):
             break
     return result
 
+
 def count_wcc(g, cap=500):
     """
     weakly connected components
     """
     return count_stat(g, nx.weakly_connected_components, cap)
+
 
 def count_scc(g, cap=500):
     """
@@ -24,24 +26,39 @@ def count_scc(g, cap=500):
     """
     return count_stat(g, nx.strongly_connected_components, cap)
 
+
 def count_sc(g, cap=500):
     """
     simple cycles
     """
     return count_stat(g, nx.simple_cycles, cap)
 
-def count_b1(g, cap=500):
-    """
-    cycle basis
-    """
-    return count_stat(g.to_undirected(), nx.cycle_basis, cap)
 
-def count_avd(g):
+def count_avd(g, cap=500):
     """
     average vertex degree
     """
     degree_values = [data[1] for data in g.degree()]
     return sum(degree_values) / len(degree_values)
+
+
+def count_e(g, cap=500):
+    return g.number_of_edges()
+
+
+def count_b0(g, cap=500):
+    g_undir = nx.Graph(g.to_undirected())
+    w = nx.number_connected_components(g_undir)
+    return w
+
+
+def count_b1(g, cap=500):
+    g_undir = nx.Graph(g.to_undirected())
+    w = nx.number_connected_components(g_undir)
+    e = g_undir.number_of_edges()
+    v = g_undir.number_of_nodes()
+    return e - v + w
+
 
 # BARCODE feature
 
@@ -50,21 +67,25 @@ def count_barcode_sum(barcode, dim):
         return 0.
     return np.sum(barcode[dim][:, 1] - barcode[dim][:, 0])
 
+
 def count_barcode_mean(barcode, dim):
     if not len(barcode[dim]):
         return 0.
     return np.mean(barcode[dim][:, 1] - barcode[dim][:, 0])
+
 
 def count_barcode_std(barcode, dim):
     if not len(barcode[dim]):
         return 0.
     return np.std(barcode[dim][:, 1] - barcode[dim][:, 0])
 
+
 def count_barcode_entropy(barcode, dim):
     if not len(barcode[dim]):
         return 0.
     lens = barcode[dim][:, 1] - barcode[dim][:, 0]
     return -np.sum(lens * np.log(np.clip(lens, 1e-9, None)))
+
 
 def count_barcode_number(barcode, dim, thresh):
     thresh = float(thresh)

topfeature_computations/utils/feature_extraction.py
@@ -20,28 +20,31 @@ def prepare_bigraph(incidence_mat, symmetric=False):
 
 
 def prepare_graph(incidence_mat):
-    return nx.from_numpy_matrix(incidence_mat.numpy(), create_using=nx.DiGraph)
+    return nx.from_numpy_matrix(incidence_mat.numpy(), create_using=nx.MultiDiGraph)
 
 
-def graph_features_from_attn(attn, thresh, used_features="wcc,scc,sc,b1,avd", use_bigraph=False):
+def graph_features_from_attn(attn, thresh, used_features="wcc,scc,sc,avd,e,b0,b1", use_bigraph=False):
     features = []
     binarized_weights = (attn > thresh).int()
     g = prepare_bigraph(binarized_weights) if use_bigraph else prepare_graph(binarized_weights)
     for feat_name in used_features.split(","):
         func = getattr(feature, f"count_{feat_name}")
-        features.append(func(g))
+        stats_value = func(g)
+        features.extend(stats_value)
+
     return features
 
 
 def graph_features_from_model(model, translator_name, src_sentence, lang_pair, head, layer, thresh, 
-                        used_features="wcc,scc,sc,b1,avd"):
+                              used_features="wcc,scc,sc,avd,e,b0,b1"):
     """
     used_features are separated by comma
-        wcc - weakly connected components
-        scc - strongly connected components
-        sc  - simple cycles
-        b1  - cycle basis
-        avd - average vertex degree
+        wcc    - weakly connected components
+        scc    - strongly connected components
+        sc     - simple cycles
+        avd    - average vertex degree
+        e      - number of edges
+        b0, b1 - betti numbers
     """
     src_lang, trg_lang = lang_pair
 
@@ -58,8 +61,15 @@ def remove_inf_barcodes(barcode):
     return barcode
 
 
+def attn_to_ripser_matrix(attn, thresh=0.0):
+    attn = (attn > thresh).int() * attn
+    attn = 1. - attn
+    attn -= np.diag(np.diag(attn))
+    return np.minimum(attn.T, attn)
+
+
 def ripser_features_from_attn(attn, used_features, maxdim=1):
-    data = rpp_py.run(f"--dim {maxdim} --format point-cloud", attn)
+    data = rpp_py.run(f"--dim {maxdim} --format distance", attn_to_ripser_matrix(attn))
 
     barcode = []
     for i in range(maxdim + 1):