c77863d302161940570581f1f64ec2c6657f1d53
Polina Guseva <p.guseva00@gmail.com>
2022-03-17 01:57:36 +0300

    add function for setting deterministic environment

--- models/src/utils/utils.py
+++ models/src/utils/utils.py
@@ -1,4 +1,18 @@
 from itertools import repeat
+import random
+
+import numpy as np
+import torch
+
+
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def inf_loop(data_loader):
