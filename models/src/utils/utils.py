a1678435fe0fa3c44c67903c39ccd8a4ee61ad49
Polina Guseva <p.guseva00@gmail.com>
2022-03-20 02:28:15 +0300

    add utils for parsing configs

--- models/src/utils/utils.py
+++ models/src/utils/utils.py
@@ -1,4 +1,5 @@
 from itertools import repeat
+import json
 import random
 
 import numpy as np
@@ -18,3 +19,18 @@ def set_seed(seed):
 def inf_loop(data_loader):
     for loader in repeat(data_loader):
         yield from loader
+
+
+def parse_config(config_path):
+    with open(config_path, "r") as in_file:
+        config = json.load(in_file)
+    return config
+
+
+def init_obj(obj_dict, module, *args, **kwargs):
+    obj_name = obj_dict["type"]
+    obj_args = dict(obj_dict["args"])
+    if len(set(obj_args.keys()) & set(kwargs.keys())) > 0:
+        raise RuntimeError("Overwriting arguments from config file is not allowed")
+    obj_args.update(kwargs)
+    return getattr(module, obj_name)(*args, **obj_args)
