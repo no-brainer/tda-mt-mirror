22eae5bdc44a712e94df56a994e7565b34194c76
Polina Guseva <p.guseva00@gmail.com>
2022-04-03 19:54:14 +0300

    add function for dataloader preparation

--- nmt_model/src/utils/utils.py
+++ nmt_model/src/utils/utils.py
@@ -1,12 +1,15 @@
 from itertools import repeat
 import json
 import random
+from typing import Dict
 
 import numpy as np
 import torch
 
+import src.datasets
 
-def set_seed(seed):
+
+def set_seed(seed: int):
     np.random.seed(seed)
     random.seed(seed)
     torch.manual_seed(seed)
@@ -34,3 +37,20 @@ def init_obj(module, obj_dict, *args, **kwargs):
         raise RuntimeError("Overwriting arguments from config file is not allowed")
     obj_args.update(kwargs)
     return getattr(module, obj_name)(*args, **obj_args)
+
+
+def prepare_dataloaders(data_params: Dict) -> Dict[str, torch.utils.data.DataLoader]:
+    dataloaders = dict()
+    for split, split_params in data_params.items():
+        datasets = [
+            init_obj(src.datasets, dataset_params) for dataset_params in split_params["datasets"]
+        ]
+
+        if len(datasets) == 1:
+            dataset = datasets[0]
+        else:
+            dataset = torch.utils.data.ConcatDataset(datasets)
+
+        dataloaders[split] = torch.utils.data.DataLoader(dataset, **split_params["dataloader"])
+
+    return dataloaders
