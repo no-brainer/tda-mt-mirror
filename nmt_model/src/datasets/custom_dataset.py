d1ea2992f03a90d903726bde381a6cb56d374f06
Polina Guseva <p.guseva00@gmail.com>
2022-04-07 02:09:55 +0300

    fix bug with max length

--- nmt_model/src/datasets/custom_dataset.py
+++ nmt_model/src/datasets/custom_dataset.py
@@ -5,11 +5,11 @@ from src.base import BaseDataset
 
 class CustomDataset(BaseDataset):
 
-    def __init__(self, src_path: str, trg_path: str, tokenizer, split: str, limit: Optional[int] = None):
+    def __init__(self, src_path: str, trg_path: str, tokenizer, split: str, max_length: int = 512, limit: Optional[int] = None):
         examples = []
         with open(src_path, "r") as src_file, \
                 open(trg_path, "r") as trg_file:
             for i, (src_sent, trg_sent) in enumerate(zip(src_file, trg_file), 1):
                 examples.append((src_sent, trg_sent))
 
-        super(CustomDataset, self).__init__(examples, tokenizer, split, limit)
+        super(CustomDataset, self).__init__(examples, tokenizer, split, max_length, limit)
