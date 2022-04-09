100891e9fa703cbc74d325228c53ff109d687f21
Polina Guseva <p.guseva00@gmail.com>
2022-04-09 18:46:40 +0300

    fix padding and normalization

--- nmt_model/src/tokenizers/utils.py
+++ nmt_model/src/tokenizers/utils.py
@@ -2,6 +2,7 @@ import os
 from typing import Dict
 
 import tokenizers
+from tokenizers.normalizers import NFD, Lowercase, StripAccents
 from tokenizers.pre_tokenizers import Whitespace
 
 
@@ -16,7 +17,13 @@ def train_new_model(model_name, model_args, trainer_args) -> tokenizers.Tokenize
             continue
         setattr(trainer, arg_name, value)
 
+    tokenizer.normalizer = tokenizers.normalizers.Sequence([NFD(), Lowercase(), StripAccents()])
     tokenizer.pre_tokenizer = Whitespace()
+
+    decoder_name = "BPEDecoder" if model_name == "BPE" else "WordPiece"
+    decoder_cls = getattr(tokenizers.decoders, decoder_name)
+    tokenizer.decoder = decoder_cls()
+
     tokenizer.train(trainer_args["train_files"], trainer)
 
     return tokenizer
