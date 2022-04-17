ad6bf6f104eb15bf4ff570af1e2e4dfdffaee0ff
Polina Guseva <p.guseva00@gmail.com>
2022-04-17 14:36:25 +0300

    improve warmup scheduler

--- nmt_model/src/schedulers/warmup_scheduler.py
+++ nmt_model/src/schedulers/warmup_scheduler.py
@@ -1,23 +1,21 @@
 class WarmupScheduler:
 
-    def __init__(self, optimizer, model_size, factor, warmup_steps):
+    def __init__(self, optimizer, model_size, max_lr, warmup_steps):
         self.optimizer = optimizer
 
         self.warmup_steps = warmup_steps
-        self.factor = factor
+        self.factor = max_lr / self._get_rate(warmup_steps)
         self.model_size = model_size
 
         self.cur_step = 0
         self.rate = None
 
+    def _get_rate(self, step):
+        return self.model_size ** -0.5 * min(step ** -0.5, step * self.warmup_steps ** -1.5)
+
     def step(self):
         self.cur_step += 1
-        step = self.cur_step
-
-        self.rate = self.factor * (
-            self.model_size ** (-0.5) *
-            min(step ** (-0.5), step * self.warmup_steps ** (-1.5))
-        )
+        self.rate = self.factor * self._get_rate(self.cur_step)
 
         for p in self.optimizer.param_groups:
             p["lr"] = self.rate
