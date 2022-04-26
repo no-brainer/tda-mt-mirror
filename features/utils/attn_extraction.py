6c36d8e2d0d34345d9e66f5a8ffb73accb99bc1b
Polina Guseva <p.guseva00@gmail.com>
2022-04-26 22:23:25 +0300

    fix small bugs in attention extraction

--- features/utils/attn_extraction.py
+++ features/utils/attn_extraction.py
@@ -20,7 +20,7 @@ class MultiheadAttentionWrapper(nn.Module):
 
     def forward(self, query, key, value, key_padding_mask=None, need_weights=True, attn_mask=None):
         attn_output, attn_output_weights = self.attn_module(
-            query, key, value, key_padding_mask=key_padding_mask, need_weights=need_weights,
+            query, key, value, key_padding_mask=key_padding_mask, need_weights=True,
             attn_mask=attn_mask, average_attn_weights=False
         )
 
@@ -28,7 +28,7 @@ class MultiheadAttentionWrapper(nn.Module):
 
 
 def prepare_for_attn_extraction(model):
-    for layer in enumerate(model.transformer.decoder.layers):
+    for layer in model.transformer.decoder.layers:
         layer.multihead_attn = MultiheadAttentionWrapper(layer.multihead_attn)
 
 
@@ -54,18 +54,19 @@ def remove_hooks(hook_handles):
 
 def get_attn_scores(sentence, translator):
     """
-    @return dict of attentions. keys are decoder.l{layer_num}. 
+    @return dict of attentions. keys are decoder.l{layer_num}.
     the shape of each tensor in dict is (1, num_heads, num_tokens_tgt, num_tokens_src)
     """
     hooks, handles = add_hooks(translator.model)
-    translator.translate(sentence)
+    try:
+        translator.translate(sentence)
 
-    attn_scores = []
-    for hook_name, hook in hooks.items():
-        attn_scores.append(hook.saved_outs[-1])
+        attn_scores = []
+        for hook_name, hook in hooks.items():
+            attn_scores.append(hook.saved_outs[-1])
+
+    finally:
+        remove_hooks(handles)
 
     attn_scores = torch.cat(attn_scores, dim=0)
-
-    remove_hooks(handles)
-
     return attn_scores.numpy()
